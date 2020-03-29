e99efbd9aa0b0910dbee1f8a0785d9271de38489
Sidney <sidneycrz@gmail.com>
2020-03-29 11:03:13 -0300

    Mobile aplicação

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,5 @@
 const express  = require('express');
+const { celebrate, Segments, Joi } = require('celebrate');
 
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
@@ -11,9 +12,22 @@ routes.post('/sessions', OngController.create);
 
 
 routes.get('/ongs', OngController.index);
-routes.post('/ongs', OngController.create);
 
-routes.get('/profile', OngController.index);
+routes.post('/ongs',celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    name: Joi.string().required(),
+    email: Joi.string().required().email(),
+    whatsapp: Joi.number().required().min(10).max(11),
+    city: Joi.string().required(),
+    uf: Joi.string().required().length(2),
+  })
+}), OngController.create);
+
+routes.get('/profile',celebrate({
+  [Segments.HEADERS]: Joi.object().keys({
+    authorization: Joi.string().required(),
+  }),
+}), ProfileController.index);
 
 routes.get('/incidents', IncidentController.index);
 routes.post('/incidents', IncidentController.create);
